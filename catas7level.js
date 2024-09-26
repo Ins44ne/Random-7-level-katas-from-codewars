9a1276de97dccfe952a5647804bd63c892157565
In44ne <ins44ne91@gmail.com>
2024-09-26 10:03:37 +0300

    feat: solution kata #7

--- catas7level.js
+++ catas7level.js
@@ -202,7 +202,58 @@ function getCount(str) {
 console.log(getCount("abracadabra"), 5);
 
 /* №7
+If　a = 1, b = 2, c = 3 ... z = 26
+Then l + o + v + e = 54
+and f + r + i + e + n + d + s + h + i + p = 108
+So friendship is twice as strong as love :-)
+Your task is to write a function which calculates the value of a word based off the sum of the alphabet positions of its characters.
+The input will always be made of only lowercase letters and will never be empty.
  */
+
+function wordsToMarks(string) {
+  const letterValues = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26,
+  };
+  let sum = 0;
+  for (let i = 0; i < string.length; i++) {
+    const letter = string[i];
+    if (letterValues[letter]) {
+      sum += letterValues[letter];
+    }
+  }
+  return sum;
+}
+
+console.log(wordsToMarks("attitude"), 100);
+console.log(wordsToMarks("friends"), 75);
+console.log(wordsToMarks("family"), 66);
+console.log(wordsToMarks("selfness"), 99);
+console.log(wordsToMarks("knowledge"), 96);
 
 /* №8
  */
