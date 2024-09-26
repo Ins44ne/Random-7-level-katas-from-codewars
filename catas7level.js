f3fae4b9e2600868042777f19d42b934982bc4cd
In44ne <ins44ne91@gmail.com>
2024-09-26 13:43:16 +0300

    feat: solution kata #13

--- catas7level.js
+++ catas7level.js
@@ -580,7 +580,30 @@ console.log(
 );
 
 /* №13
+Create a function named divisors/Divisors that takes an integer n > 1 and returns an array with all of the integer's divisors(except for 1 and the number itself), 
+from smallest to largest. If the number is prime return the string '(integer) is prime' 
+(null in C#, empty table in COBOL) (use Either String a in Haskell and Result<Vec<u32>, String> in Rust).
+Examples:
+divisors(12) --> [2, 3, 4, 6]
+divisors(25) --> [5]
+divisors(13) --> "13 is prime"
  */
+
+function divisors(int) {
+  let arr = [];
+  for (let i = 2; i < int; i += 1) {
+    if (int % i === 0) {
+      arr.push(i);
+    }
+  }
+  return arr.length > 0 ? arr : `${int} is prime`;
+}
+
+console.log("integer = 15", () => assert.deepEqual(divisors(15), [3, 5]));
+console.log("integer = 12", () => assert.deepEqual(divisors(12), [2, 3, 4, 6]));
+console.log("integer = 13", () =>
+  assert.deepEqual(divisors(13), "13 is prime")
+);
 
 /* №14
  */
