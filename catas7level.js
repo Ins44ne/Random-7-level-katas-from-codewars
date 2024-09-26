7b960f5ed3169c7b97033aaea821c268e538b2ef
In44ne <ins44ne91@gmail.com>
2024-09-26 14:11:31 +0300

    feat: solution kata #13

--- catas7level.js
+++ catas7level.js
@@ -599,14 +599,37 @@ function divisors(int) {
   return arr.length > 0 ? arr : `${int} is prime`;
 }
 
-console.log("integer = 15", () => assert.deepEqual(divisors(15), [3, 5]));
-console.log("integer = 12", () => assert.deepEqual(divisors(12), [2, 3, 4, 6]));
-console.log("integer = 13", () =>
-  assert.deepEqual(divisors(13), "13 is prime")
-);
+console.log((divisors(15), [3, 5]));
+console.log((divisors(12), [2, 3, 4, 6]));
+console.log((divisors(13), "13 is prime"));
 
 /* №14
+Kata Task
+I have a cat and a dog which I got as kitten / puppy.
+I forget when that was, but I do know their current ages as catYears and dogYears.
+Find how long I have owned each of my pets and return as a list [ownedCat, ownedDog]
+NOTES:
+Results are truncated whole numbers of "human" years
+  Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+  Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that
  */
+
+var ownedCatAndDog = function (catYears, dogYears) {
+  return [
+    catYears === 15 ? 1 : catYears === 15 ? 2 : (catYears - 24) / 4 + 2,
+    dogYears === 15 ? 1 : dogYears === 15 ? 2 : (dogYears - 24) / 5 + 2,
+  ];
+};
+
+console.log(ownedCatAndDog(15, 15), [1, 1]);
+console.log(ownedCatAndDog(24, 24), [2, 2]);
+console.log(ownedCatAndDog(56, 64), [10, 10]);
 
 /* №15
  */
