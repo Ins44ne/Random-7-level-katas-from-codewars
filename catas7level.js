a16cba13659f17358025a38203077a0d20aaeb2f
In44ne <ins44ne91@gmail.com>
2024-09-27 11:55:28 +0300

    feat: solution kata #16

--- catas7level.js
+++ catas7level.js
@@ -675,7 +675,41 @@ console.log(hexColor("212 103 212"), "magenta");
 console.log(hexColor("101 101 092"), "yellow");
 
 /* №16
+This time no story, no theory. The examples below show you how to write function accum:
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+The parameter of accum is a string which includes only letters from a..z and A..Z.
  */
+
+function accum(s) {
+  return s
+    .split("")
+    .map((char, index) => char.toUpperCase() + char.toLowerCase().repeat(index))
+    .join("-");
+}
+
+console.log(
+  accum("ZpglnRxqenU"),
+  "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+);
+console.log(
+  accum("NyffsGeyylB"),
+  "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+);
+console.log(
+  accum("MjtkuBovqrU"),
+  "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+);
+console.log(
+  accum("EvidjUnokmM"),
+  "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+);
+console.log(
+  accum("HbideVbxncC"),
+  "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
+);
 
 /* №17
  */
