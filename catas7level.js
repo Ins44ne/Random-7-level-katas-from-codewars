3b34425215deac00bd1108e640e0b981584d20bf
In44ne <ins44ne91@gmail.com>
2024-09-27 12:13:07 +0300

    feat: solution kata #17

--- catas7level.js
+++ catas7level.js
@@ -712,7 +712,26 @@ console.log(
 );
 
 /* №17
+Given a string and an array of integers representing indices, capitalize all letters at the given indices.
+For example:
+capitalize("abcdef",[1,2,5]) = "aBCdeF"
+capitalize("abcdef",[1,2,5,100]) = "aBCdeF". There is no index 100.
+The input will be a lowercase string with no spaces and an array of digits.
  */
+
+function capitalize(s, arr) {
+  return s
+    .split("")
+    .map((e, i) => (arr.includes(i) ? e.toUpperCase() : e))
+    .join("");
+}
+
+console.log(capitalize("abcdef", [1, 2, 5]), "aBCdeF");
+console.log(capitalize("abcdef", [1, 2, 5, 100]), "aBCdeF");
+console.log(capitalize("codewars", [1, 3, 5, 50]), "cOdEwArs");
+console.log(capitalize("abracadabra", [2, 6, 9, 10]), "abRacaDabRA");
+console.log(capitalize("codewarriors", [5]), "codewArriors");
+console.log(capitalize("indexinglessons", [0]), "Indexinglessons");
 
 /* №18
  */
