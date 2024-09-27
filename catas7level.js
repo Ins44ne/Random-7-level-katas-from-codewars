ccd90aa4cfa95cd4e5928c51f172ca4a4230f91e
In44ne <ins44ne91@gmail.com>
2024-09-27 13:16:48 +0300

    feat: solution kata #20

--- catas7level.js
+++ catas7level.js
@@ -771,7 +771,40 @@ function multiples(m, n) {
 console.log(multiples(3, 5), [5, 10, 15]);
 
 /* №20
+A trick I learned in elementary school to determine whether or not a number was divisible by three is to add all of the integers in the number together and to divide the resulting sum by three. If there is no remainder from dividing the sum by three, then the original number is divisible by three as well.
+Given a series of digits as a string, determine if the number represented by the string is divisible by three.
+Example:
+"123"      -> true
+"8409"     -> true
+"100853"   -> false
+"33333333" -> true
+"7"        -> false
  */
+
+function divisibleByThree(str) {
+  return (str.split("").reduce((sum, el) => sum + Number(el), 0) / 3) % 1 === 0;
+}
+
+console.log(
+  divisibleByThree("123"),
+  true,
+  "Should return true if the sum of the given digits is divisible by 3."
+);
+console.log(
+  divisibleByThree("19254"),
+  true,
+  "Should return true if the sum of the given digits is divisible by 3."
+);
+console.log(
+  divisibleByThree("88"),
+  false,
+  "Should return false if the sum of the given digits is not divisible by 3."
+);
+console.log(
+  divisibleByThree("1"),
+  false,
+  "Should return false if the sum of the given digits is not divisible by 3."
+);
 
 /* №21
  */
