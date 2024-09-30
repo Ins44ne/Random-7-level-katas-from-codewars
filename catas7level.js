8ee376c2064fbb3a94d03c46f968f31e8015c77f
In44ne <ins44ne91@gmail.com>
2024-09-30 14:15:57 +0300

    feat: solution kata #26

--- catas7level.js
+++ catas7level.js
@@ -944,8 +944,26 @@ console.log(password("Ab1!@#$%^&*()-_+={}[]|:;?/>.<,"), true);
 console.log(password("!@#$%^&*()-_+={}[]|:;?/>.<,"), false);
 
 /* №26
+Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
+For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1. (81-1-1-81)
+  Example #2: An input of 765 will/should return 493625 because 72 is 49, 62 is 36, and 52 is 25. (49-36-25)
+Note: The function accepts an integer and returns an integer.
  */
-console.log;
+
+function squareDigits(num) {
+  let arr = num
+    .toString()
+    .split("")
+    .map((el) => {
+      return Math.pow(Number(el), 2);
+    });
+  return Number(arr.join(""));
+}
+
+console.log(squareDigits(3212), 9414);
+console.log(squareDigits(2112), 4114);
+console.log(squareDigits(0), 0);
+
 /* №27
  */
 console.log;
