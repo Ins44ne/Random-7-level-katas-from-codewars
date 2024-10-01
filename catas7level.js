642127f3ac93ea3db601afcc2e7bedca08ba0a22
In44ne <ins44ne91@gmail.com>
2024-10-01 15:23:21 +0300

    feat: solution kata #33

--- catas7level.js
+++ catas7level.js
@@ -1138,8 +1138,49 @@ console.log(
 console.log(broken("100010"), "011101");
 
 /* №33
+The Chinese zodiac is a repeating cycle of 12 years, with each year being represented by an animal and its reputed attributes.
+The lunar calendar is divided into cycles of 60 years each, and each year has a combination of an animal and an element.
+There are 12 animals and 5 elements; the animals change each year, and the elements change every 2 years.
+The current cycle was initiated in the year of 1984 which was the year of the Wood Rat.
+Since the current calendar is Gregorian, I will only be using years from the epoch 1924.
+For simplicity I am counting the year as a whole year and not from January/February to the end of the year.
+  ##Task
+Given a year, return the element and animal that year represents ("Element Animal"). For example I'm born in 1998 so I'm an "Earth Tiger".
+  animals (or $animals in Ruby) is a preloaded array containing the animals in order:
+['Rat', 'Ox', 'Tiger', 'Rabbit', 'Dragon', 'Snake', 'Horse', 'Goat', 'Monkey', 'Rooster', 'Dog', 'Pig']
+  elements (or $elements in Ruby) is a preloaded array containing the elements in order:
+['Wood', 'Fire', 'Earth', 'Metal', 'Water']
  */
-console.log;
+
+function chineseZodiac(year) {
+  const animals = [
+    "Rat",
+    "Ox",
+    "Tiger",
+    "Rabbit",
+    "Dragon",
+    "Snake",
+    "Horse",
+    "Goat",
+    "Monkey",
+    "Rooster",
+    "Dog",
+    "Pig",
+  ];
+  const elements = ["Wood", "Fire", "Earth", "Metal", "Water"];
+  return `${elements[Math.floor(((((year - 1984) % 10) + 10) % 10) / 2)]} ${
+    animals[(((year - 1984) % 12) + 12) % 12]
+  }`;
+}
+
+console.log(chineseZodiac(1965), "Wood Snake");
+console.log(chineseZodiac(1938), "Earth Tiger");
+console.log(chineseZodiac(1998), "Earth Tiger");
+console.log(chineseZodiac(2016), "Fire Monkey");
+console.log(chineseZodiac(1924), "Wood Rat");
+console.log(chineseZodiac(1968), "Earth Monkey");
+console.log(chineseZodiac(2162), "Water Dog");
+
 /* №34
  */
 console.log;
