7b2740599ce341c6574c9fd54ed95360a939c7c9
In44ne <ins44ne91@gmail.com>
2024-10-01 14:14:52 +0300

    feat: solution kata #31

--- catas7level.js
+++ catas7level.js
@@ -1094,8 +1094,28 @@ console.log(getDivisorsCnt(11), 2);
 console.log(getDivisorsCnt(54), 8);
 
 /* №31
+Given an array of numbers, return the difference between the largest and smallest values.
+  For example:
+[23, 3, 19, 21, 16] should return 20 (i.e., 23 - 3).
+[1, 434, 555, 34, 112] should return 554 (i.e., 555 - 1).
+The array will contain a minimum of two elements. Input data range guarantees that max-min will cause no integer overflow.
  */
-console.log;
+
+function betweenExtremes(numbers) {
+  return Math.max.apply(null, numbers) - Math.min.apply(null, numbers);
+}
+
+console.log(
+  betweenExtremes([21, 34, 54, 43, 26, 12]),
+  42,
+  "betweenExtremes([21, 34, 54, 43, 26, 12])"
+);
+console.log(
+  betweenExtremes([-1, -41, -77, -100]),
+  99,
+  "betweenExtremes([-1, -41, -77, -100])"
+);
+
 /* №32
  */
 console.log;
