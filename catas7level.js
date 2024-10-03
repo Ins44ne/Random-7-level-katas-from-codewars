f640d2653c0620c640f1338d321b137e69595b7c
In44ne <ins44ne91@gmail.com>
2024-10-03 10:25:50 +0300

    feat: solution kata #36

--- catas7level.js
+++ catas7level.js
@@ -1230,8 +1230,31 @@ console.log(descendingOrder(1021), 2110);
 console.log(descendingOrder(123456789), 987654321);
 
 /* №36
+Create a function that takes a number and returns an array of strings containing the number cut off at each digit.
+  Examples
+420 should return ["4", "42", "420"]
+2017 should return ["2", "20", "201", "2017"]
+2010 should return ["2", "20", "201", "2010"]
+4020 should return ["4", "40", "402", "4020"]
+80200 should return ["8", "80", "802", "8020", "80200"]
+PS: The input is guaranteed to be an integer in the range [0, 1000000]
  */
-console.log;
+
+function createArrayOfTiers(num) {
+  let res = [];
+  let elem = "";
+  String(num)
+    .split("")
+    .forEach((el) => {
+      elem += el;
+      res.push(elem);
+    });
+  return res;
+}
+
+console.log(createArrayOfTiers(420), ["4", "42", "420"]);
+console.log(createArrayOfTiers(2017), ["2", "20", "201", "2017"]);
+
 /* №37
  */
 console.log;
