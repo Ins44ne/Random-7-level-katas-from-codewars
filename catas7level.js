1051aa74ecbbea2ed467e27ddb142589421ae0c0
In44ne <ins44ne91@gmail.com>
2024-10-03 09:54:50 +0300

    feat: solution kata #35

--- catas7level.js
+++ catas7level.js
@@ -1206,8 +1206,29 @@ console.log(
 );
 
 /* №35
+Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+  Examples:
+Input: 42145 Output: 54421
+Input: 145263 Output: 654321
+Input: 123456789 Output: 987654321
  */
-console.log;
+
+function descendingOrder(n) {
+  return Number(
+    String(n)
+      .split("")
+      .sort((a, b) => b - a)
+      .join("")
+  );
+}
+
+console.log(descendingOrder(0), 0);
+console.log(descendingOrder(1), 1);
+console.log(descendingOrder(111), 111);
+console.log(descendingOrder(15), 51);
+console.log(descendingOrder(1021), 2110);
+console.log(descendingOrder(123456789), 987654321);
+
 /* №36
  */
 console.log;
