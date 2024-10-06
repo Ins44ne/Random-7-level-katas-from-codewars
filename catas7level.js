55913c04327ce979a79c0fd767a0f0ed76be7f44
In44ne <ins44ne91@gmail.com>
2024-10-06 20:32:33 +0300

    feat: solution kata #44

--- catas7level.js
+++ catas7level.js
@@ -1443,9 +1443,41 @@ console.log(last(1, 2, 3), 3, "last(1, 2, 3)");
 console.log(last("a", "b", "c"), "c", "last('a', 'b', 'c')");
 console.log(last([1], [2], [3]), [3], "last([1], [2], [3])");
 console.log(last(1, 2, 3, [4, 5]), [4, 5], "last(1, 2, 3, [4, 5])");
+
 /* №44
+Task
+There're k square apple boxes full of apples. If a box is of size m, then it contains m × m apples. You noticed two interesting properties about the boxes:
+  The smallest box is of size 1, 
+  the next one is of size 2,..., 
+  all the way up to size k.
+Boxes that have an odd size contain only yellow apples. 
+Boxes that have an even size contain only red apples.
+Your task is to calculate the difference between the number of red apples and the number of yellow apples.
+  Example
+For k = 5, the output should be -15
+There are 1 + 3 × 3 + 5 × 5 = 35 yellow apples and 2 × 2 + 4 × 4 = 20 red apples, thus the answer is 20 - 35 = -15.
+  Input/Output
+[input] integer k
+A positive integer.
+Constraints: 1 ≤ k ≤ 40
+[output] an integer
+The difference between the two types of apples.
  */
-console.log;
+
+function appleBoxes(k) {
+  let sumYel = 0;
+  let sumRed = 0;
+  for (let i = 1; i <= k; i += 1) {
+    i % 2 === 0 ? (sumRed += i * i) : (sumYel += i * i);
+  }
+  return sumRed - sumYel;
+}
+
+console.log(appleBoxes(5), -15, "appleBoxes(5)");
+console.log(appleBoxes(15), -120, "appleBoxes(15)");
+console.log(appleBoxes(36), 666, "appleBoxes(36)");
+console.log(appleBoxes(1), -1, "appleBoxes(1)");
+
 /* №45
  */
 console.log;
