a889447abe51622c1f211dcb9ed049b26fe259d8
In44ne <ins44ne91@gmail.com>
2024-10-07 22:15:31 +0300

    feat: solution kata #45

--- catas7level.js
+++ catas7level.js
@@ -1479,8 +1479,42 @@ console.log(appleBoxes(36), 666, "appleBoxes(36)");
 console.log(appleBoxes(1), -1, "appleBoxes(1)");
 
 /* №45
+In this Kata, you will be given a string and your task will be to return a list of ints detailing the count of uppercase letters, lowercase, numbers and special characters (everything else), as follows.
+The order is: uppercase letters, lowercase letters, numbers and special characters.
+  "*'&ABCDabcde12345" --> [ 4, 5, 5, 3 ]
+More examples in the test cases.
  */
-console.log;
+
+function solve(s) {
+  let upper = 0,
+    lower = 0,
+    digits = 0,
+    special = 0;
+
+  for (let char of s) {
+    if (char >= "A" && char <= "Z") {
+      upper++;
+    } else if (char >= "a" && char <= "z") {
+      lower++;
+    } else if (char >= "0" && char <= "9") {
+      digits++;
+    } else {
+      special++;
+    }
+  }
+
+  return [upper, lower, digits, special];
+}
+
+console.log(solve(""), [0, 0, 0, 0]);
+console.log(solve("aAbBcC"), [3, 3, 0, 0]);
+console.log(solve("Codewars@codewars123.com"), [1, 18, 3, 2]);
+console.log(solve("bgA5<1d-tOwUZTS8yQ"), [7, 6, 3, 2]);
+console.log(solve("P*K4%>mQUDaG$h=cx2?.Czt7!Zn16p@5H"), [9, 9, 6, 9]);
+console.log(solve("RYT'>s&gO-.CM9AKeH?,5317tWGpS<*x2ukXZD"), [15, 8, 6, 9]);
+console.log(solve("$Cnl)Sr<7bBW-&qLHI!mY41ODe"), [10, 7, 3, 6]);
+console.log(solve("@mw>0=QD-iAx!rp9TaG?o&M%l$34L.nbft"), [7, 13, 4, 10]);
+
 /* №46
  */
 console.log;
